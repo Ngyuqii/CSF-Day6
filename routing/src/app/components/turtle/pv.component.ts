dd6ada24027ea151718a5dfd6d39228382246a07
ngyuqi <ngyuqi89@gmail.com>
2023-03-27 20:05:17 +0800

    Commit

--- routing/src/app/components/turtle/pv.component.ts
+++ routing/src/app/components/turtle/pv.component.ts
@@ -13,19 +13,34 @@ export class PvComponent implements OnInit, OnDestroy {
   num = "";
   param$! : Subscription;
 
+  name! : string[];
+  queryParams$! :  Subscription;
+
   constructor(private activatedRoute: ActivatedRoute){ };
 
   ngOnInit(): void {
+      //1. Path Variable
+      //http://localhost:4200/adoptaturtle/{pvId}
       this.param$ = this.activatedRoute.params.subscribe(
         params => {
           this.num= params["pvId"];
           console.log(this.num);
         }
       );
+
+      //2. Query Param
+      //http://localhost:4200/adoptaturtle/{pvId}?name=name1|name2|name3
+      this.queryParams$ = this.activatedRoute.queryParams.subscribe(
+        (queryParams) => {
+          this.name = queryParams['name'].split('|');
+          console.log(this.name);
+        }
+      );
   }
 
   ngOnDestroy(): void {
       this.param$.unsubscribe();
+      this.queryParams$.unsubscribe();
   }
 
 }
